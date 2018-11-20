668c11f6085eab133ceb3456b68c93c590d64f59
umiko <robertscholl.private@gmail.com>
2018-11-20 18:35:56 +0100

    improved drawable object and made small changes all over

--- DrawableObject.js
+++ DrawableObject.js
@@ -1,23 +1,38 @@
 class DrawableObject{
+
+    shaderPrograms = [];
+    shaderProgramInUse = 0;
+
+
     constructor(context, vertCode, fragCode, vertBuffer){
-        this.shaderProgram = WebGLForFun.initShader(context, vertCode, fragCode);
+        this.addShaderProgram(context, vertCode, fragCode);
         this.vertBuffer = vertBuffer;
     }
 
+    addShaderProgram(context, vertCode, fragCode, setActive=false){
+        let shaderProgram = WebGLForFun.initShader(context, vertCode,fragCode);
+        this.shaderPrograms.add(shaderProgram);
+        if(setActive){
+            this.shaderProgramInUse = this.shaderPrograms.indexOf(shaderProgram);
+        }
+    }
+
     draw(context){
-        context.useProgram(this.shaderProgram);
-        context.enableVertexAttribArray(context.getAttribLocation(this.shaderProgram, "position"));
-        context.vertexAttribPointer(context.getAttribLocation(this.shaderProgram, "position"), 2, context.FLOAT, false, 0,0);
+        context.useProgram(this.shaderPrograms[this.shaderProgramInUse]);
+        context.enableVertexAttribArray(context.getAttribLocation(this.shaderPrograms[this.shaderProgramInUse], "position"));
+        context.vertexAttribPointer(context.getAttribLocation(this.shaderPrograms[this.shaderProgramInUse], "position"), 2, context.FLOAT, false, 0,0);
         context.drawArrays(context.TRIANGLES, 0,3);
-        context.disableVertexAttribArray(context.getAttribLocation(this.shaderProgram, "position"));
+        context.disableVertexAttribArray(context.getAttribLocation(this.shaderPrograms[this.shaderProgramInUse], "position"));
     }
 
     destroy(context){
-        context.getAttachedShaders(this.shaderProgram).map(shader => {
-            context.detachShader(this.shaderProgram, shader);
-            context.deleteShader(shader);
+        this.shaderPrograms.map(shaderProgram => {
+            context.getAttachedShaders(shaderProgram).map(shader => {
+                context.detachShader(shaderProgram, shader);
+                context.deleteShader(shader);
+            });
+            context.deleteProgram(shaderProgram);
         });
-        context.deleteProgram(this.shaderProgram);
         context.deleteBuffer(this.vertBuffer);
     }
 }
--- WebGLFramework.js
+++ WebGLFramework.js
@@ -6,24 +6,38 @@ class WebGLForFun {
     }
 
     static initWebGL(viewportCanvas) {
-        console.log("initWebGL");
+        //retrieve context
+        let context = viewportCanvas.getContext('webgl', {alpha:true, depth:true});
+        //check if a context has been retrieved
+        if(!context){
+            console.log("Acquiring context failed, falling back on experimental-webgl...");
+            context = viewportCanvas.getContext('experimental-webgl');
+        }
+        //if it still does not have a context, give the user a hint
+        if(!context){
+            alert("lmao get a real browser");
+            return false;
+        }
 
-        const context = viewportCanvas.getContext('webgl', {alpha:true, depth:true});
-
+        //set initial values
         context.clearColor(0, 0, 0, 1);
         context.clearDepth(1.0);
         context.viewport(0,0, viewportCanvas.width, viewportCanvas.height);
         context.enable(context.DEPTH_TEST);
 
+        //return the prepared context
         return context;
     }
-
+    //create a shader program and return it
     static initShader(context, vertCode, fragCode){
-        console.log("initshaders");
         const shaderProgram =  context.createProgram();
 
         const compiledVertShader = WebGLForFun.compileShader(context, vertCode, context.VERTEX_SHADER);
         const compiledFragShader = WebGLForFun.compileShader(context, fragCode, context.FRAGMENT_SHADER);
+
+        if(!compiledFragShader || !compiledVertShader){
+            console.error("Cannot continue shader program creation. At least one of the shaders did not compile")
+        }
 
         context.attachShader(shaderProgram, compiledVertShader);
         context.attachShader(shaderProgram, compiledFragShader);
@@ -31,7 +45,7 @@ class WebGLForFun {
         context.linkProgram(shaderProgram);
 
         if(!context.getProgramParameter(shaderProgram, context.LINK_STATUS)){
-            throw "Unable to link!\n" + context.getProgramInfoLog(shaderProgram);
+            throw "Unable to link shader program!\n" + context.getProgramInfoLog(shaderProgram);
         }
 
         context.useProgram(shaderProgram);
@@ -45,10 +59,9 @@ class WebGLForFun {
         context.shaderSource(shader, shaderSource);
         context.compileShader(shader);
         if(!context.getShaderParameter(shader, context.COMPILE_STATUS)){
-            console.log(shaderSource);
-            throw "Compilation of shader unsuccessful.\n" + context.getShaderInfoLog(shader);
+            console.error("Compilation of shader unsuccessful.\n" + context.getShaderInfoLog(shader)+"\n"+shaderSource);
+            return null;
         }
-        console.log("Shader compiled!");
         return shader;
     }
 
@@ -59,6 +72,7 @@ class WebGLForFun {
         return buffer;
     }
 
+    //clear the canvas
     static clear(context){
         context.clear(context.COLOR_BUFFER_BIT | context.DEPTH_BUFFER_BIT);
     }
